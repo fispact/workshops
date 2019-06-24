6f22ca4c32bb3e90d6095bc8c7274c10ab47e740
thomasms <stainer.tom@gmail.com>
2019-06-24 13:41:19 +0100

    Fix to gamma spec animation script

--- 2019/exercises/advanced/readgg/gammaspectrumanimation.py
+++ 2019/exercises/advanced/readgg/gammaspectrumanimation.py
@@ -8,6 +8,10 @@ import matplotlib.animation as animation
 
 filename = 'inventory.out'
 
+bounds = np.linspace(1e-11, 5, 20000, endpoint=True)        
+# overwrite bounds with input since we know that precision causes issues here
+energy = [ (bounds[j] + bounds[j+1])/2.0 for j in range(0, len(bounds) -1) ]
+
 with pp.Reader(filename) as output:
     count = len(output)
     
@@ -20,29 +24,31 @@ with pp.Reader(filename) as output:
                 maxEnergy = max(maxEnergy, max(t.gamma_spectrum.boundaries))
             
         fig, ax = plt.subplots()
-
-        bounds = np.linspace(1e-11, 5, 20000, endpoint=True)        
-        line, = ax.plot([], [])
+        line, = plt.plot(energy, [0.0]*len(energy))
 
         def animate(i):
             gs = output[i].gamma_spectrum
-            # overwrite bounds with input since we know that precision causes issues here
-            energy = [ (bounds[j] + bounds[j+1])/2.0 for j in range(0, len(bounds) -1) ]
             values = gs.values
-            line.set_xdata(energy)  # update the data
-            line.set_ydata(values)  # update the data
+            if len(values) == 0:
+                values = [0.0]*len(energy)
+                
+            line.set_data(energy, values)
+            assert len(energy) == len(values)
             return line,
 
-        ani = animation.FuncAnimation(fig, animate, np.arange(0, count),
-                                      interval=200, blit=True)
-        # save animation at 30 frames per second 
-        #ani.save('gammaspec.gif', writer='imagemagick', fps=30)
-
+        ani = animation.FuncAnimation(fig, animate,
+                                      frames=count,
+                                      interval=200,
+                                      repeat_delay=400,
+                                      blit=True)
         plt.xlim(0, maxEnergy)
         plt.ylim(max(1e1, maxValue*1e-8), max(maxValue*1.2, 1e2))
         plt.xlabel('ENERGY (MeV)')
         plt.ylabel('GAMMA RAY POWER FROM ACTIVATION DECAY (MeV/s)')
         plt.yscale('log')
         plt.grid()
+
+        # save animation at 30 frames per second 
+#        ani.save('gammaspec.gif', writer='imagemagick', fps=30)
 
         plt.show()
